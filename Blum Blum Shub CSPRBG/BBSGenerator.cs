48bf314d36621861b0c254213ba5ad0a93e27fa1
Matei Gabriel Anita <matei.anita98@gmail.com>
2020-05-30 21:31:28 +0300

    Added the ability to generate a random string of bits

---
+++ Blum Blum Shub CSPRBG/BBSGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Blum_Blum_Shub_CSPRBG
+{
+    class BBSGenerator
+    {
+        private static BigInteger previousNumber;
+        private static BigInteger p;
+        private static BigInteger q;
+        private static BigInteger m;
+        private static bool parametersSet = false;
+        private static long currentBit;
+
+        private static bool ValidateParameters(BigInteger p, BigInteger q)
+        {
+            return true;
+        }
+
+        public static bool SetParameters(BigInteger p, BigInteger q)
+        {
+            if (ValidateParameters(p, q) == false)
+                return false;
+            m = p * q;
+            BBSGenerator.p = p;
+            BBSGenerator.q = q;
+            Random random = new Random();
+            previousNumber = random.Next((int)(m - 1));
+            parametersSet = true;
+
+            return true;
+        }
+
+        private static char GetRandomBit()
+        {
+            char randomBit;
+            previousNumber = BigInteger.ModPow(previousNumber, 2, m);
+            randomBit = (char)BigInteger.ModPow(previousNumber,1,2);
+            return randomBit;
+        }
+
+        public static String GetRandomBitString(long size)
+        {
+            StringBuilder bitString = new StringBuilder();
+            currentBit = 0;
+
+            while (currentBit < size)
+            {
+                currentBit++;
+                bitString.Append(GetRandomBit());
+            }
+            //bazinga
+            int x;
+            return bitString.ToString();
+        }
+    }
+}
--- Blum Blum Shub CSPRBG/MainWindow.xaml.cs
+++ Blum Blum Shub CSPRBG/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Numerics;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
 
 namespace Blum_Blum_Shub_CSPRBG
 {
@@ -24,6 +27,64 @@ namespace Blum_Blum_Shub_CSPRBG
         public MainWindow()
         {
             InitializeComponent();
+            textBoxPInput.Text = "11";
+            textBoxQInput.Text = "23";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BigInteger p, q;
+            double size;
+
+            String fileName = "output_" + DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss") + ".txt";
+            String filePath = textBoxFolderInput.Text + "\\" + fileName;
+            BinaryWriter binaryWriter;
+
+
+            try
+            {
+                size = Double.Parse(textBoxFileSize.Text);
+                binaryWriter = new BinaryWriter(File.Open(filePath, FileMode.Create));
+                p = BigInteger.Parse(textBoxPInput.Text);
+                q = BigInteger.Parse(textBoxPInput.Text);
+            }
+            catch (Exception ex)
+            {
+                if (ex is FormatException ||
+                    ex is NotSupportedException)
+                {
+                    textBoxErrors.Text = "Incorrect input introduced in one of the boxes.";
+                    return;
+                }
+
+                throw;
+            }
+
+            textBoxErrors.Text = "";
+            BBSGenerator.SetParameters(p, q);
+            String randomBitString = BBSGenerator.GetRandomBitString((long)(size * 1000000));
+            binaryWriter.Write(randomBitString);
+            binaryWriter.Close();
+            textBoxErrors.Text = randomBitString.Length.ToString();
+        }
+        
+        private void checkBoxParameters_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxQInput.IsEnabled = textBoxPInput.IsEnabled = checkBoxParameters.IsChecked == true ? false : true;
+        }
+
+        private void SelectFolder(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+
+
+            CommonFileDialogResult result = dialog.ShowDialog();
+            if (result == CommonFileDialogResult.Ok)
+            {
+                textBoxFolderInput.Text = dialog.FileName;
+            }
+
         }
     }
 }
